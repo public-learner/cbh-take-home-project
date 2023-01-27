99bf76c116b719c7b6fa3ef9d731d9ae6b139af1
sweetjustin0717@gmail.com <askarovdavron2@gmail.com>
2023-01-27 15:54:21 +0900

    updated testing cases

--- dpk.test.js
+++ dpk.test.js
@@ -7,7 +7,13 @@ describe("deterministicPartitionKey", () => {
   });
 
   it("Returns the literal '123' when given 123", () => {
-    const trivialKey = deterministicPartitionKey({partitionKey: "123"});
-    expect(trivialKey).toBe("123");
+    const simpleKey = deterministicPartitionKey({partitionKey: "123"});
+    expect(simpleKey).toBe("123");
+  });
+
+  it("Returns the literal 128 characters when given over 256 over key", () => {
+    let partitionKey = 'a'.repeat(300);
+    const trivialKey = deterministicPartitionKey({partitionKey: partitionKey});
+    expect(trivialKey).toHaveLength(128);
   });
 });
